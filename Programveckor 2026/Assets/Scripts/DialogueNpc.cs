2b4126b4f26a119f10f197cce00c5b65d7fa491e
NorTheGoon67 <nor.ericsson@elev.ga.lbs.se>
2026-01-12 11:58:27 +0100

    dialogue3

--- Programveckor 2026/Assets/Scripts/DialogueNpc.cs
+++ Programveckor 2026/Assets/Scripts/DialogueNpc.cs
@@ -1,25 +1,61 @@
 using UnityEngine;
-
-
 public class DialogueNpc : MonoBehaviour
+
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     bool isPlayerNearby = false;
-    public GameObject dialogueUI;
+    public GameObject DialogueNpcGame;
+
+
+
     void Start()
     {
 
     }
 
+
+    private bool Dopen = false;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && isPlayerNearby == true)
         {
-            print("Spelaren vill prata med Npc");
-            dialogueUI.SetActive(true);
+            ShowDialogue();
         }
+
+
     }
+    void ShowDialogue()
+    {
+        if (Dopen) return;
+        Dopen = true;
+        // Dopen = Dialogue open
+
+        DialogueNpcGame.SetActive(true);
+    }
+
+    void OnYesClicked()
+    {
+        Debug.Log("Spelaren valde att prata med NPC:n!");
+        DialogueNpcGame.SetActive(false);
+
+        // forsätt här i från
+
+
+        Time.timeScale = 1f;
+        // Här kan du starta NPC-dialogen, t.ex. öppna ny textsekvens
+    }
+
+    void OnNoClicked()
+    {
+        Debug.Log("Spelaren valde att inte prata.");
+        DialogueNpcGame.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("NPC i närheten");
@@ -28,7 +64,7 @@ public class DialogueNpc : MonoBehaviour
     void OnTriggerExit2D(Collider2D collision)
     {
         isPlayerNearby = false;
-        dialogueUI.SetActive(false);
+        DialogueNpcGame.SetActive(false);
     }
 
 
