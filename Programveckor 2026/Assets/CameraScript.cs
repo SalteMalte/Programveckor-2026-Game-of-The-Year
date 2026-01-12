b582abca9db10b333772cf42f64d06bd113dcd32
SalteMalte <malte.skoglund@elev.ga.lbs.se>
2026-01-12 10:09:36 +0100

    clara test

---
+++ Programveckor 2026/Assets/CameraScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraScript : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Invoke("scenechanger", 1.5f);
+        }
+
+
+
+
+    }
+    public void scenechanger()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
