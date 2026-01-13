f0214b5efe4f7384ab6915ed525c8c4f01850919
KoflopKoflop <leo.holmstrom@elev.ga.lbs.se>
2026-01-13 09:38:41 +0100

    added spawner for button

--- Programveckor 2026/Assets/Scripts/Button appearance.cs
+++ Programveckor 2026/Assets/Scripts/Button appearance.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Buttonappearance : MonoBehaviour
 {
+    [SerializeField]
+    Transform spawnLocation;
+    [SerializeField]
+    GameObject prefab;
+    public Renderer myrenderer2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
 
+    }
+    public float timer = 0;
+    bool squareVisible = false;
     // Update is called once per frame
     void Update()
     {
-        
+        myrenderer2.enabled = false;
+
+        timer -= Time.deltaTime;
+
+        Vector2 pos = transform.position;
+        if (spawnLocation != null)
+        {
+            pos = spawnLocation.position;
+        }
+        if (timer < 0 && !squareVisible)
+        {
+            GameObject spawnedObject = Instantiate(prefab, pos, Quaternion.identity);
+            squareVisible = true;
+        }
+
     }
 }
