185979fccfdc8dc5a9ea19973960beea6d5f1e88
SalteMalte <malte.skoglund@elev.ga.lbs.se>
2026-01-13 12:10:14 +0100

    mergar

--- Programveckor 2026/Assets/ChangeBackToMapFromPuzzle.cs
+++ Programveckor 2026/Assets/ChangeBackToMapFromPuzzle.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeBackToMapFromPuzzle : MonoBehaviour
 {
-    public static bool CompletedPuzzleOne;
+    public static bool CompletedPuzzleOne = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
