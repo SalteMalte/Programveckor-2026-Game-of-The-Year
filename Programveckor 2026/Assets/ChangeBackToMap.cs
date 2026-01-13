556763d02bb7209133ae8b570043129b6db73799
SalteMalte <malte.skoglund@elev.ga.lbs.se>
2026-01-13 10:14:40 +0100

    Added Switching to Camera view etc.
    I added switching to camera view, i made it so that you save your player position between scenes so that you return to your original place after opening the camera, i also added so that if youre in the right position, youre able to start the puzzle. I also made it so that the start of the puzzle takes you to the map aswell as resetting the PlayerPrefs

---
+++ Programveckor 2026/Assets/ChangeBackToMap.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeBackToMap : MonoBehaviour
+{
+    public static Vector3 PlayerPosition;
+    public static int PlayerScene;
+    float x = 3;
+    float y = 4;
+
+    private string SceneToGo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Invoke("scenechangerbacktomap", 0.5f);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            print(x + " " + y);
+            if (PlayerPosition.y < y + 1 && PlayerPosition.y > y - 1 && PlayerPosition.x < x + 1 && PlayerPosition.x > x - 1 && ChangeBackToMap.PlayerScene == 3)
+            {
+                Invoke("scenechangetopuzzle1", 0.5f);
+            }
+        }
+    }
+    public void scenechangerbacktomap()
+    {
+        //spara position
+        PlayerPrefs.SetFloat("posX", PlayerPosition.x);
+        PlayerPrefs.SetFloat("posY", PlayerPosition.y);
+        SceneManager.LoadScene(ChangeBackToMap.PlayerScene);
+    }
+    public void scenechangetopuzzle1()
+    {
+        //spara position
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        try
+        {
+            SceneToGo = collision.gameObject.GetComponent<PuzzleObject>().SceneName;
+        }
+        catch { }
+    }
+}
